70a02e9b6e7b0fdec29ced3151549bfbc9577ebc
NezrinIsmayilli <86964192+NezrinIsmayilli@users.noreply.github.com>
2022-06-30 15:13:16 +0400

    website

---
+++ Muse Website/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+
+    margin:10,
+    
+    responsive:{
+        0:{
+            items:3,
+            
+        },
+        600:{
+            items:3,
+          
+        },
+        1000:{
+            items:6,
+           
+        }
+    }
+
+
+    });
+
+  });
+
+
+
